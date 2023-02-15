d0aa36a5b7a21a3ed79af3b65a365f6cd1c34042
FreddieKrugerrand <freddiekrugerrand@protonmail.com>
2023-02-15 16:55:13 -0500

    Add max hops calculation for base case

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,57 @@
 use lightning::util::ser::{BigSize, Writeable};
 
+static PAYLOAD_LIMIT: usize = 1300;
+
 fn main() {
-    println!("Onion Sizer {}", tlv_size(BigSize(1000), false, 0));
+    let example = max_hops(BigSize(1000), true, 0);
+    println!("Onion Sizer {} hops / {} filler", example.0, example.1);
+}
+
+// Returns the maximum number of hops that will fit in the onion payload and
+// the bytes in the filler.
+fn max_hops(amount: BigSize, is_mpp: bool, metadata_len: usize) -> (usize, usize) {
+    // Start with payload for our final hop (which will have mpp and metadata
+    // fields).
+    let final_payload_tlvs = tlv_size(&amount, is_mpp, metadata_len);
+    let final_payload_total = payload_size(final_payload_tlvs);
+
+    // On the off chance that our final payload exceeds the limit, we can't
+    // have any hops.
+    if final_payload_total > PAYLOAD_LIMIT {
+        return (0, PAYLOAD_LIMIT);
+    }
+
+    let mut hops = 1;
+    let mut used_bytes = final_payload_total;
+
+    while used_bytes <= PAYLOAD_LIMIT {
+        // For intermediate hops, we don't include any mpp fields or metadata.
+        let intermediate_payload_tlvs = tlv_size(&amount, false, 0);
+        let intermediate_payload_total = payload_size(intermediate_payload_tlvs);
+
+        // If this payload would
+        if used_bytes + intermediate_payload_total > PAYLOAD_LIMIT {
+            return (hops, PAYLOAD_LIMIT - used_bytes);
+        }
+
+        // If we could fit our bytes within the remaining limit add them to
+        // our total and increment hops.
+        used_bytes += intermediate_payload_total;
+        hops += 1;
+    }
+
+    (hops, PAYLOAD_LIMIT - used_bytes)
+}
+
+fn payload_size(tlv_total: usize) -> usize {
+    // The total size for an individual payload is:
+    // - A BigSize for the length of the payload.
+    // - All the TLV fields.
+    // - 32 bytes for a hmac.
+    let with_hmac = tlv_total + 32;
+    let len_bytes = BigSize(with_hmac.try_into().unwrap()).encode().len();
+
+    with_hmac + len_bytes
 }
 
 // Calculates the total bytes to store the TLVs in a payload, based on the
@@ -15,7 +65,7 @@ fn main() {
 // * is_mpp: the final payload will need an extra 32 bytes for a payment_secret
 //   and to repeat the payment amount.
 // * metadata: arbitrary data for the final hop can have any size.
-fn tlv_size(amount: BigSize, is_mpp: bool, metadata_len: usize) -> usize {
+fn tlv_size(amount: &BigSize, is_mpp: bool, metadata_len: usize) -> usize {
     // Start with our payload size and field count assuming that we have a
     // short_channel_id, which is 8 bytes.
     let mut payload_bytes: usize = 8;
